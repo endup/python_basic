7bfb52d2635a8b46ad759c3a6c469a3dde422dc0
for the future <32151591+endup@users.noreply.github.com>
2017-10-24 17:10:12 +0800

    datetime
    pass

---
+++ 小型库的使用/datetime/datetime_lib.py
@@ -0,0 +1,29 @@
+﻿from datetime import datetime,timedelta
+
+now=datetime.now()
+print(now)
+print(now.strftime('%a, %b %d %H:%M'))
+print(now + timedelta(days=2, hours=12))
+
+dt = datetime(2015, 4, 19, 12, 20)
+print(dt)
+#print(dt.timestamp())
+
+"""
+# 拿到UTC时间，并强制设置时区为UTC+0:00:
+>>> utc_dt = datetime.utcnow().replace(tzinfo=timezone.utc)
+>>> print(utc_dt)
+2015-05-18 09:05:12.377316+00:00
+# astimezone()将转换时区为北京时间:
+>>> bj_dt = utc_dt.astimezone(timezone(timedelta(hours=8)))
+>>> print(bj_dt)
+2015-05-18 17:05:12.377316+08:00
+# astimezone()将转换时区为东京时间:
+>>> tokyo_dt = utc_dt.astimezone(timezone(timedelta(hours=9)))
+>>> print(tokyo_dt)
+2015-05-18 18:05:12.377316+09:00
+# astimezone()将bj_dt转换时区为东京时间:
+>>> tokyo_dt2 = bj_dt.astimezone(timezone(timedelta(hours=9)))
+>>> print(tokyo_dt2)
+2015-05-18 18:05:12.377316+09:0
+"""
